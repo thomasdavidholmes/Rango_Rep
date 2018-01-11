93b1f6862cfb1b35aa39636eee5f1a911d29f258
thomasdavidholmes <33161811+thomasdavidholmes@users.noreply.github.com>
2018-01-11 20:19:58 +0000

    phase2

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,4 +2,12 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner! <br/><a href='/rango/about/'>About</a>")
+
+def about(request):
+    return HttpResponse("""<html><head><title>About</title><head><body>
+            <h1>What does Rango say </h1>
+        Rango says some shite <a href="/rango/">Index</a>
+        </body>
+    </html>
+    """)
