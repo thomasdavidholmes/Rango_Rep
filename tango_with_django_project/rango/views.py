346e17f9f9a478cc572c2fa3fba057600589897c
thomasdavidholmes <33161811+thomasdavidholmes@users.noreply.github.com>
2018-01-12 11:03:25 +0000

    Push after img on about

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -7,9 +7,4 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("""<html><head><title>About</title><head><body>
-            <h1>What does Rango say </h1>
-        Rango says some shite <a href="/rango/">Index</a>
-        </body>
-    </html>
-    """)
+    return render(request, 'rango/about.html')
